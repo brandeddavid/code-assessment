ee62a81ca87001c667e2e7baf4fa3400a02194be
David Mwangi <david.mathenge98@gmail.com>
2022-12-14 23:37:48 +0300

    feat: add and test addToList function

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,10 +1,36 @@
 import * as utils from '../utils';
+import { addToList } from '../utils';
 
 describe('utils', () => {
   describe('utils#getMessage()', () => {
     it('should return a message', () => {
       const message = utils.getMessage();
       expect(message).toEqual('Hello world!');
+    });
+  });
+
+  describe('utils#addToList()', () => {
+    const validPayload = { name: 'Name', age: 20 };
+
+    it('should return error if no name is provided', () => {
+      const nameError = utils.addToList({ name: '', age: null });
+      expect(nameError).toEqual('Name is required');
+    });
+
+    it('should return error if age below 18', () => {
+      const ageError = utils.addToList({ name: 'Name', age: 17 });
+      expect(ageError).toEqual('Need to be over 18');
+    });
+
+    it('should return error if age is above 100', () => {
+      const ageError = utils.addToList({ name: 'Name', age: 101 });
+      expect(ageError).toEqual('Invalid age given');
+    });
+
+    it('should handle successful add', () => {
+      const response = utils.addToList(validPayload);
+      expect(response.length).toEqual(1);
+      expect(response[0]).toMatchObject(validPayload);
     });
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,29 @@
-export function getMessage(): string {
-  return 'Hello world!';
-}
+type ListItem = {
+  name: string;
+  age?: number | null;
+};
+const list: ListItem[] = [];
+
+const getMessage = (): string => 'Hello world!';
+
+const addToList = (item: ListItem): string | ListItem[] => {
+  const { name, age } = item;
+
+  if (!name) {
+    return 'Name is required';
+  }
+
+  if (age && age < 18) {
+    return 'Need to be over 18';
+  }
+
+  if (age && age > 100) {
+    return 'Invalid age given';
+  }
+
+  list.push(item);
+
+  return list;
+};
+
+export { getMessage, addToList };
